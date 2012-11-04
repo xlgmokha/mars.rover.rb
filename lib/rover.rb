85e494c3f7ecfe98f44ca969cdcc372929e92488
mo k <mo@mokhan.ca>
2012-11-04 14:11:24 -0700

    move Location to a separate file.

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,3 +1,4 @@
+require 'location'
 class Rover
   def initialize(heading, x, y, plateau)
     @plateau = plateau
@@ -22,22 +23,5 @@ class Rover
 
   def to_s
     @location.to_s
-  end
-end
-
-class Location
-  attr_reader :location, :heading
-  def initialize(x, y, heading)
-    @location = {:x => x, :y => y}
-    @heading = heading
-  end
-  def rotate(degrees)
-    @heading = @heading.rotate(degrees)
-  end
-  def is_facing(direction)
-    @heading.represents? direction
-  end
-  def to_s
-    "#{@location[:x]} #{@location[:y]} #{@heading}"
   end
 end
