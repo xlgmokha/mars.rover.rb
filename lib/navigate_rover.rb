22137d5b03461eba01caf2115774c1a2583d2ead
mo k <mo@mokhan.ca>
2012-11-04 12:31:26 -0700

    use multiple variable assignment.

--- lib/navigate_rover.rb
+++ lib/navigate_rover.rb
@@ -6,15 +6,13 @@ require "turn_right"
 class NavigateRover
   def initialize(plateau_size, starting_position, instructions)
     @plateau_size = plateau_size
-    @x = starting_position.split()[0].to_i
-    @y = starting_position.split()[1].to_i
-    @heading = starting_position.split()[2]
-    @instructions = instructions
+    @x, @y, @heading = starting_position.split()
+    @instructions = instructions.split(//)
   end
   def run
-    rover = create_plateau(@plateau_size).deploy_rover_to(@heading, @x, @y)
-    @instructions.split(//).each do |instruction|
-      commands_for(rover).each do |item| 
+    rover = create_plateau(@plateau_size).deploy_rover_to(@heading, @x.to_i, @y.to_i)
+    @instructions.each do |instruction|
+      commands_for(rover).each do |item|
         item.run(instruction)
       end
     end
