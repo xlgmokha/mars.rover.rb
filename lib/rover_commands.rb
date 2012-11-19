bc99a1bf3dc232dcafa43876b255ad175efab727
mo k <mo@mokhan.ca>
2012-11-19 14:03:36 -0700

    make rover extend Rotation module.

--- lib/rover_commands.rb
+++ lib/rover_commands.rb
@@ -1,11 +1,13 @@
 class RCommand
   def run(rover)
-    rover.rotate(90)
+    rover.extend(Rotation)
+    rover.turn_right
   end
 end
 class LCommand
   def run(rover)
-    rover.rotate(-90)
+    rover.extend(Rotation)
+    rover.turn_left
   end
 end
 class MCommand
